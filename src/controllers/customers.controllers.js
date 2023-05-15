bb725bc30d8312996db33304cd7d83230ed8cf0e
Sergio Beppu <beppu.sergio@gmail.com>
2023-05-15 12:29:02 -0300

    correções para o HUB

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -18,6 +18,8 @@ export async function getCustomersByID(req, res) {
         const customer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id])
         if(customer.rowCount === 0) return res.sendStatus(404)
 
+        customer.rows.map(el => el.birthday = dayjs(el.birthday).format('YYYY-MM-DD'))
+
         return res.send(customer.rows[0])
     } catch (err) {
         console.log(err.message)
@@ -47,8 +49,9 @@ export async function updateCustomer (req, res) {
     const {id} = req.params
 
     try {
-        const notCorrectCustomer = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])
-        if(notCorrectCustomer.rowCount > 0) return res.sendStatus(409)
+        const notCorrectCustomer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id])
+        const notCorrectCustomerCpf = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])
+        if(notCorrectCustomer.rowCount === 0 || notCorrectCustomerCpf.rows[0].id != id) return res.sendStatus(409)
         await db.query(`UPDATE customers SET name=$1, phone=$2, birthday=$3 WHERE id=$4`, [name, phone, birthday, id])
         res.sendStatus(200)
         
